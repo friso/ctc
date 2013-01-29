d95638cf207ad480d41731f37d9d4d5cf009101f
Friso van Vollenhoven <fvanvollenhoven@xebia.com>
2013-01-29 17:53:46 +0100

    Works better now.

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -14,23 +14,34 @@ stopwords = nltk.corpus.stopwords.words('english')
 stopwords.extend(['also', ''])
 
 
+def normed_tf_idf(term_frequencies, st):
+	return (term_frequencies[st] / float(max(term_frequencies.values()))) / (math.log(len(document_frequencies)) / float(document_frequencies[st]))
 
 """
 Return the TDIDF score for each term in the given document.
 Possibly update the global state.
 """
-def get_tfidf(document, update_state = False):
+def get_tf_idf(document, update_state = False):
 	tokenizer = nltk.RegexpTokenizer("\w+")
 	stemmer = nltk.PorterStemmer()
 
+	terms = filter(lambda t: t not in stopwords, tokenizer.tokenize(document))
+
+	stemmed_to_terms = {}
+	for term in terms:
+		stem = stemmer.stem(term.lower())
+		cnt = stemmed_to_terms.get(stem, Counter())
+		cnt.update([term])
+		stemmed_to_terms[stem] = cnt
+
+	stemmed_to_terms = { stemmer.stem(t) : t for t in terms }
+	term_frequencies = Counter([stemmer.stem(t) for t in terms])
+	document_frequencies.update(stemmed_to_terms.keys())
+	tf_idfs = [{ 'score':normed_tf_idf(term_frequencies, t), 'term':stemmed_to_terms[t].most_common(1)[0][0] } for t in term_frequencies.keys()]
+	tf_idfs = sorted(tf_idfs, key = lambda x: x['score'], reverse = True)
+	return tf_idfs
 
 
-	terms = map(lambda w: (w, stemmer.stem(w.lower())), filter(lambda t: t not in stopwords, tokenizer.tokenize(document)))
-	term_frequencies = Counter(map(lambda x: x[1], terms))
-	document_frequencies.update(map(lambda x: x[1], terms))
-
-	#tfidf with normed tf
-	result = map(lambda t: (t[0], t[1], (term_frequencies[t[1]] / float(max(term_frequencies.values()))) * (math.log(len(document_frequencies)) / float(document_frequencies[t[1]]))), terms)
 		
 
 
