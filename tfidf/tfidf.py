293946793b48091e708bd412a945a798d3a1bf13
Joris Bontje <jorisbontje@gmail.com>
2013-01-29 20:28:24 +0100

    custom stopwords because we know better

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -10,9 +10,7 @@ Some global state here. Don't tell me about using globals. I know!
 """
 document_frequencies = Counter()
 
-stopwords = nltk.corpus.stopwords.words('english')
-stopwords.extend(['also', ''])
-
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'also', '']
 
 def normed_tf_idf(term_frequencies, st):
     tf = term_frequencies[st] / float(max(term_frequencies.values()))
