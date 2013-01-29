54e6a4823883f5b8b79c003cb6941144eb789325
Joris Bontje <jorisbontje@gmail.com>
2013-01-29 20:22:19 +0100

    start small

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -1,3 +1,4 @@
+import bz2
 import nltk
 import re
 import math
@@ -14,47 +15,49 @@ stopwords.extend(['also', ''])
 
 
 def normed_tf_idf(term_frequencies, st):
-	tf = term_frequencies[st] / float(max(term_frequencies.values()))
-	idf = math.log((len(document_frequencies) or 1) / float(document_frequencies.get(st)) + 1.0)
-	return tf * idf
+    tf = term_frequencies[st] / float(max(term_frequencies.values()))
+    idf = math.log((len(document_frequencies) or 1) / float(document_frequencies.get(st, 1)) + 1.0)
+    return tf * idf
 
 """
 Return the TDIDF score for each term in the given document.
 Possibly update the global state.
 """
 def get_tf_idf(document, update_state = False):
-	tokenizer = nltk.RegexpTokenizer("\w+")
-	stemmer = nltk.PorterStemmer()
+    tokenizer = nltk.RegexpTokenizer("\w+")
+    stemmer = nltk.PorterStemmer()
 
-	terms = filter(lambda t: t not in stopwords, tokenizer.tokenize(document))
+    terms = filter(lambda t: t not in stopwords, tokenizer.tokenize(document))
 
-	stemmed_to_terms = {}
-	for term in terms:
-		stem = stemmer.stem(term.lower())
-		cnt = stemmed_to_terms.get(stem, Counter())
-		cnt.update([term])
-		stemmed_to_terms[stem] = cnt
+    stemmed_to_terms = {}
+    for term in terms:
+        stem = stemmer.stem(term.lower())
+        cnt = stemmed_to_terms.get(stem, Counter())
+        cnt.update([term])
+        stemmed_to_terms[stem] = cnt
 
-	term_frequencies = Counter([stemmer.stem(t.lower()) for t in terms])
+    term_frequencies = Counter([stemmer.stem(t.lower()) for t in terms])
 
-	if update_state:
-		document_frequencies.update(stemmed_to_terms.keys())
+    if update_state:
+        document_frequencies.update(stemmed_to_terms.keys())
 
-	tf_idfs = [{ 'score':normed_tf_idf(term_frequencies, t), 'term':stemmed_to_terms[t].most_common(1)[0][0] } for t in term_frequencies.keys()]
-	tf_idfs = sorted(tf_idfs, key = lambda x: x['score'], reverse = True)
-	return tf_idfs
+    tf_idfs = [{ 'score':normed_tf_idf(term_frequencies, t), 'term':stemmed_to_terms[t].most_common(1)[0][0] } for t in term_frequencies.keys()]
+    tf_idfs = sorted(tf_idfs, key = lambda x: x['score'], reverse = True)
+    return tf_idfs
 
 def extract_term_frequencies(tokens):
-	result = {}
-	for token in tokens:
-		result[token] = result.get(token, 0) + 1
-	return result
+    result = {}
+    for token in tokens:
+        result[token] = result.get(token, 0) + 1
+    return result
 
 def init():
-	with file('corpus.txt') as f:
-		lines = f.readlines()
-		# blank lines start a new paragraph
-		# paragraphs of 30 words or less are filtered out; this excludes things like titles and blank paragraphs (due to double blank lines)
-		paragraphs = filter(lambda p: len(p.strip().split(' ')) > 30, reduce(lambda x, y: x + (y if len(y) > 1 else '\n'), map(lambda x: x.strip() + ' ', lines)).split('\n'))
-		for p in paragraphs:
-			get_tf_idf(p, True)
+    print "loading... ",
+    with file('corpus_1000.txt') as f:
+        paragraph = ""
+        for line in f.readlines():
+            paragraph += line
+            if line.strip() == "":
+                get_tf_idf(paragraph, True)
+                paragraph = ""
+    print "done"
